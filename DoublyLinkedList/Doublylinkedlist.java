ef35ffe7708fddf40b27e46d6ca7917200cd1ac8
SivaTejaArla <sivatejaarla.com>
2024-11-05 06:33:05 +0530

    h

--- DoublyLinkedList/Doublylinkedlist.java
+++ DoublyLinkedList/Doublylinkedlist.java
@@ -63,6 +63,33 @@ public class Doublylinkedlist {
         }
        
     }
+    public void insertPos(int data,int p){
+        Node newNode =new Node(data);
+        Node c = head;
+        int count = 0;
+        while(c!=null){
+            c=c.next;
+            count++;
+        }
+        if(p==1)
+        insertAtBeginning(data);
+        else if(p == count + 1)
+        insertEnd(data);
+        else{
+            Node a = head;
+            int cn = 1;
+            while(cn< p-1){
+                a =a.next;
+                cn = cn+1;
+            }
+            Node f = a.next;
+            newNode.prev = a;
+            a.next = newNode;
+            f.prev = newNode;
+            newNode.next = f;
+        }
+
+    }
 
     void deleteFirst() {
         if (head == null) {
@@ -75,9 +102,10 @@ public class Doublylinkedlist {
 
         else {
             head.next.prev = null;
+            head = head.next;
+            temp.next = null;
         }
-        head = head.next;
-        temp.next = null;
+       
     }
 
     void deleteLast() {
@@ -101,10 +129,14 @@ public class Doublylinkedlist {
         Doublylinkedlist l = new Doublylinkedlist();
         l.insertAtBeginning(4);
         l.display();
+        
         l.insertAtBeginning(8);
         l.display();
-        l.insertEnd(6);
+        l.insertPos(6,3);
         l.display();
+        l.deleteLast();
+        l.display();
+       
 
 
     }
