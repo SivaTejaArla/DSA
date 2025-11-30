35f100ca12c10ffd3549af0c9c6cb2347bcb0bef
sivatejaarla <sivatejaarla@gmail.com>
2025-11-30 16:32:01 +0530

    two methods added

--- Recursion/SortedArray.java
+++ Recursion/SortedArray.java
@@ -3,21 +3,30 @@ package Recursion;
 //In this problem we will check if an array is sorted using recursion.
 public class SortedArray {
 
-    static boolean isSorted(int a[], int i, int j) {
+    static boolean isSortedBack(int a[], int i, int j) { // checking from back of array
         if (a.length == 0)
             return false;
 
         if (i < 0)
             return true;
         if (a[j] > a[i]) {
-            return isSorted(a, i - 1, j - 1);
+            return isSortedBack(a, i - 1, j - 1);
         } else
             return false;
 
     }
 
+    static boolean isSortedFront(int []a, int i){ // checking from front 
+
+        if(a.length == 0) return false; 
+        if(i +1 > a.length-1) return true;
+        if(a[i] < a[i+1]) return isSortedFront(a, i+1);
+        else return false;
+    }
+
     public static void main(String[] args) {
-        int[] a = { 1, 2, 3, 4, 5 };
-        System.out.print(isSorted(a, a.length - 2, a.length - 1));
+        int[] a = { 1, 2, 3, 14, 5 };
+        System.out.println(isSortedBack(a, a.length - 2, a.length - 1));
+         System.out.println(isSortedFront(a,0));
     }
 }
