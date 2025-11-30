5fee0ec811feef307a4e056d7d9ee823c06a98bc
sivatejaarla <sivatejaarla@gmail.com>
2025-11-30 18:38:19 +0530

    recursion added in bubble sort

--- SortingAlgorithms/Bubblesort.java
+++ SortingAlgorithms/Bubblesort.java
@@ -20,48 +20,47 @@ OUTER LOOP
 
  */
 
-
 import java.util.Arrays;
-public class Bubblesort{
-    
-    public void printArray(int[] a){        //Printingarray
+
+public class Bubblesort {
+
+    public void printArray(int[] a) { // Printingarray
 
         System.out.println();
-       System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a));
     }
-    public void bubbleSort(int[] a){
-        for(int i =0 ;i<a.length-1;i++){
-            for(int j =0;j<a.length-1;j++)
-            {
-                if(a[j]>a[j+1]){            //Swapping without temp
-                    a[j] +=a[j+1];
-                    a[j+1] = a[j] - a[j+1];
-                    a[j] = a[j] - a[j+1];
+
+    public void bubbleSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) { // Swapping without temp
+                    a[j] += a[j + 1];
+                    a[j + 1] = a[j] - a[j + 1];
+                    a[j] = a[j] - a[j + 1];
                 }
             }
         }
     }
 
+    public void bubbleSortRecursion(int[] a, int n) {
 
-public void bubbleSortRecursion(int[] a ,int n){
-
-if(n==1) return;
-    for(int i = 0 ; i < a.length-1 ; i++){
-        if(a[i] > a[i+1]){
-            a[i] +=a[i+1];
-            a[i+1] = a[i] - a[i+1]; 
-            a[i] = a[i] - a[i+1];
+        if (n == 1)     // which means we are at the last loop or at last element
+            return;
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                a[i] += a[i + 1];
+                a[i + 1] = a[i] - a[i + 1];
+                a[i] = a[i] - a[i + 1];
+            }
         }
+        bubbleSortRecursion(a, n - 1);
+
     }
-bubbleSortRecursion(a,n-1);
-
-}
-
 
     public static void main(String[] args) {
         Bubblesort b = new Bubblesort();
-        int[] a = {1,7,5,9,3,2,11,67,34 };
-                int[] aa = {1,7,5,9,3,2,11,67,34 };
+        int[] a = { 1, 7, 5, 9, 3, 2, 11, 67, 34 };
+        int[] aa = { 1, 7, 5, 9, 3, 2, 11, 67, 34 };
 
         b.printArray(a);
         b.bubbleSort(a);
@@ -69,7 +68,6 @@ bubbleSortRecursion(a,n-1);
         b.bubbleSortRecursion(aa, aa.length);
         b.printArray(aa);
 
-
     }
-    
+
 }
