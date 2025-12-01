4b843967158db58b300b8c9d5a925596709a1a89
SivaTejaArla <sivatejaarla@gmail.com>
2025-12-01 16:53:38 +0530

    Refactor character elimination methods in Java

--- Recursion/eliminateCharacters.java
+++ Recursion/eliminateCharacters.java
@@ -2,24 +2,31 @@
 In this problem we need to eliminate the specific characters from the given string and return the output using Recursion.
 
 */
+
 import java.util.*;
 
 class Main {
     
     static StringBuilder resultString(String a, int n, StringBuilder result){
-        if(a.length() == 0) return new StringBuilder("Invalid Input");  // Check for the valid input 
-        if(n>=a.length()) return result;   //Return result after all the loops
-        if(a.charAt(n) != 'a' && a.charAt(n) != 'A') result.append(a.charAt(n)); // Checking for both Lower Case and Upper case
-        resultString(a,n+1,result);   // recursion call
+        if(a.length() == 0) return new StringBuilder("Invalid Input");
+        if(n>=a.length()) return result; 
+        if(a.charAt(n) != 'a' && a.charAt(n) != 'A') result.append(a.charAt(n));
+        resultString(a,n+1,result);
         
         return result;
     }
-    
+    static String usingSubString(String a) {
+        if(a.isEmpty()) return "";
+        char c = a.charAt(0);
+        if(c == 'a') return usingSubString(a.substring(1));
+        else return c+usingSubString(a.substring(1));
+    }
     
     public static void main(String[] args) {
         
-        String a = "";
+        String a = "ghshgahaaa";
         StringBuilder result =new StringBuilder();
         System.out.println(resultString(a,0,result));
+        System.out.println(usingSubString(a));
     }
 }
