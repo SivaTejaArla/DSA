80674725e77e8a5e663e4b8c798c0b4968825af1
SivaTejaArla <sivatejaarla@gmail.com>
2026-07-22 12:47:48 +0530

    Create Pattern.java

---
+++ Patterns/Pattern.java
@@ -0,0 +1,39 @@
+/*
+patterns are mostly nested loops only 
+
+** Outer Loop - For rows 
+** Inner Loops - For Columns 
+**Print "*" in inner loop only 
+**Observe the Symmetry                      
+
+****
+****
+****
+for(int i = 0 ; i <3 ; i++){
+
+for(int j = 0 ; j <4 ; j++) print("*")
+}
+
+
+
+*/
+
+
+class Pattern{
+
+
+    public static void main(String []args){
+    
+    for(int i = 0 ; i <3 ; i++){
+
+            for(int j = 0 ; j <4 ; j++) {
+
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+}
+    
+    }
+
+
+}
