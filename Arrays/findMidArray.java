9b142b298c3e8e5d358a2f70343675933bb8c826
SivaTejaArla <sivatejaarla@gmail.com>
2026-07-31 21:04:28 +0530

    Remove unnecessary blank line in findMidArray.java

--- Arrays/findMidArray.java
+++ Arrays/findMidArray.java
@@ -8,7 +8,6 @@ Write a function called middle that takes an array and returns a new array that 
 */
 
 
-
 import java.util.Arrays;
 public class findMidArray {
     public static int[] middle(int[] array) {
