8b272ee45568190ab920fbe312eb1df0827c7d1d
SivaTejaArla <sivatejaarla@gmail.com>
2026-07-31 21:04:17 +0530

    Add import statement for java.util.Arrays

--- Arrays/findMidArray.java
+++ Arrays/findMidArray.java
@@ -8,6 +8,7 @@ Write a function called middle that takes an array and returns a new array that 
 */
 
 
+
 import java.util.Arrays;
 public class findMidArray {
     public static int[] middle(int[] array) {
